d5ed06cf81a1dacbe9b55bbddad869b0fc7205d8
Habib-0007 <109099706+Habib-0007@users.noreply.github.com>
2023-04-09 02:59:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ document.getElementById("getResult").onclick = function (){
     let x;
     let y;
     
-    a = document.getElementById("firstValue").value;
+    a = eval(document.getElementById("firstValue").value);
     a = Number(a);
     
-    b = document.getElementById("secondValue").value;
+    b = eval(document.getElementById("secondValue").value);
     b = Number(b);
 
-    c = document.getElementById("thirdValue").value;
+    c = eval(document.getElementById("thirdValue").value);
     c = Number(c);
     
     m = Math.sqrt(Math.pow(b,2) - (4 * a * c));
     x = (- b + m) / (2 * a);
     y = (- b - m) / (2 * a);
-    document.getElementById("firstResult").innerHTML = "The value of x is: " + x + " or " + y;  
+    document.getElementById("firstResult").innerText = "The value of x is: " + x + " or " + y;  
     
 }
 
@@ -38,22 +38,22 @@ document.getElementById("getEquationResult").onclick = function () {
     let y;
     
     
-    a = document.getElementById("coA").value;
+    a = eval(document.getElementById("coA").value);
     a = Number(a);
     
-    b = document.getElementById("coB").value;
+    b = eval(document.getElementById("coB").value);
     b = Number(b);
     
-    e = document.getElementById("firstConstant").value;
+    e = eval(document.getElementById("firstConstant").value);
     e = Number(e);
 
-    c = document.getElementById("coC").value;
+    c = eval(document.getElementById("coC").value);
     c = Number(c);
 
-    d = document.getElementById("coD").value;
+    d = eval (document.getElementById("coD").value);
     d = Number(d);
     
-    f = document.getElementById("secondConstant").value;
+    f = eval (document.getElementById("secondConstant").value);
     f = Number(f);
     
     g = (b * f) - (d * e);
@@ -63,9 +63,47 @@ document.getElementById("getEquationResult").onclick = function () {
     x = g / i;
     y = h / i;
     
-    document.getElementById("oneResult").innerHTML = "x = " + x;
+    document.getElementById("oneResult").innerText = "x = " + x;
 
-document.getElementById("twoResult").innerHTML = "y = " + y;
-    
+document.getElementById("twoResult").innerText = "y = " + y;
     
 }
+
+
+let threeLinear = document.querySelector("#getThreeLinearRes");
+
+threeLinear.addEventListener("click", () => {
+    let a = eval(document.querySelector("#valA").value);
+    let b = eval(document.querySelector("#valB").value);
+    let c = eval(document.querySelector("#valC").value);
+    let d = eval(document.querySelector("#valD").value);
+    let e = eval(document.querySelector("#valE").value);
+    let f = eval(document.querySelector("#valF").value);
+    let g = eval(document.querySelector("#valG").value);
+    let h = eval(document.querySelector("#valH").value);
+    let i = eval(document.querySelector("#valI").value);
+    let j = eval(document.querySelector("#valJ").value);
+    let k = eval(document.querySelector("#valK").value);
+    let l = eval(document.querySelector("#valL").value);
+    
+    let den =( ( a * (f * k - g * j) ) - ( b * (e * k - g * i) ) + ( c * (e * j - i * f) ) );
+    
+   let xNum =( ( d * (f * k - g * j) ) - ( b * (h * k - g * l) ) + ( c * (h * j - l * f) ) );
+   
+   let yNum =( ( a * (h * k - g * l) ) - ( d * (e * k - g * i) ) + ( c * (e * l - i * h) ) );
+   
+   let zNum =( ( a * (f * l - h * j) ) - ( b * (e * l - h * i) ) + ( d * (e * j - i * f) ) );
+     
+     let x = (xNum/den);
+     let y = (yNum/den);
+     let z = (zNum/den);
+     
+     let xValue = document.querySelector("#valDRes");
+     let yValue = document.querySelector("#valHRes");
+     let zValue = document.querySelector("#valLRes");
+    
+     xValue.innerText = `x = ${x}`;
+     yValue.innerText = `y = ${y}`;
+     zValue.innerText = `z = ${z}`;
+     
+});
